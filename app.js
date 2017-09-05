3e13969a7f496465188d93220ef690ef104ba650
theroich <theroich@gmail.com>
2017-09-05 21:21:00 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,65 @@
+let q = require('q');
+let mqtt = require('mqtt');
+let events = require('events');
+const util = require('util');
+
+
+function ClientFactory(options){
+    var topicState= `stat/${options.name}/POWER`;
+    var topicPower = `cmnd/${options.name}/power`;
+
+    events.EventEmitter.call(this);
+
+    let self = this;
+    let client  = mqtt.connect(options.host,{
+        port: options.port,
+        username: options.username,
+        password: options.password,
+        clientId: 'mqttjs_' + Math.random().toString(16).substr(2, 8)
+    });
+
+    client.on('connect', function () {
+
+        client.subscribe(topicState);
+        client.subscribe(topicPower);
+        self.emit('connect');
+        client.on('message', function (topic, message) {
+
+            if(topicPower == topic && message.toString() == 'ON'){
+                self.emit('toggleON');
+
+            }else if(topicPower == topic && message.toString() == 'OFF'){
+                self.emit('toggleOFF')
+            }
+
+        });
+
+    });
+
+    this.toggleOn = function(){
+        client.publish(topicPower,'ON');
+    };
+
+    this.toggleOff = function(){
+        client.publish(topicPower,'OFF');
+    };
+
+
+
+    this.getState = function(){
+        var deferred = q.defer();
+        client.publish(topicPower,'');
+
+        client.on('message', function (topic, message) {
+           if(topicState == topic){
+               deferred.resolve(message.toString());
+           }
+
+        });
+        return deferred.promise;
+    }
+
+}
+util.inherits(ClientFactory, events.EventEmitter);
+
+module.exports = ClientFactory;
